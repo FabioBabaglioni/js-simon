876b7ff885d99b40034aaa21dbcfc3661e056574
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-18 17:23:31 +0100

    correzzione funzione clock

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 
-let btn = document.getElementById("button")
 // creare una funzione per creare 5 numeri casuali da 1 a 99
-
-
 for(let i=0; i<5; i++){
     randNumber();
 }
 
 setTimeout(clock, 3000);
 
-clock()
-
 
 // lasciare la possibilità all'utente di ricopiare i numeri
-btn.addEventListener("click",
-
-check()
-
-);
+let btn = document.getElementById("button")
+btn.addEventListener("click", check);
 
 // dare il risuktato vedendo quali numeri sono giusti e quali errati
+
+for(let i = 0; i<5; i++){
+    if(arrNumUtente[i] === arrNum[i]){
+        count[i]
+        console.log(count)
+    }
+
+}
+
 
 
