ffbd776f87ffdfb5cb6d3d3fdcd05e01bcab0d4b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-18 15:16:58 +0100

    creazione codice base e commenti

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// creare una funzione per creare 5 numeri casuali da 1 a 99
+
+
+
+// creare un timer di 30 secondi
+
+
+
+// far scomparire i numeri 
+
+
+// lasciare la possibilità all'utente di ricopiare i numeri
+
+
+// dare il risuktato vedendo quali numeri sono giusti e quali errati
+
+
