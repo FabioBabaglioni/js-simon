b6f4a9c1c48975c75a9731ad7a6db297960b745d
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-18 15:26:40 +0100

    Creaxione dei 5 numeri casuali

---
+++ js/function.js
@@ -0,0 +1,7 @@
+
+let random
+
+function randNumber(number){
+    Math.floor(Math.random()*100)
+    return number
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // creare una funzione per creare 5 numeri casuali da 1 a 99
-
+for(let i=0; i<5; i++){
+   let randomNumber = Math.floor(Math.random()*100)
+    console.log(randomNumber)
+}
 
 
 // creare un timer di 30 secondi
